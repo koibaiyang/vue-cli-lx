6bc2fa66348acb1b538e755baded880e45d072d7
koibaiyang <2586056022@qq.com>
2023-05-10 22:37:07 +0800

    新增了vue-cli

---
+++ src/Test.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p>cs1</p>
+        <p>{{username}}</p>
+        <p>{{age}}</p>
+        <button @click="cn">改变</button>
+    </div>
+</template>
+
+    <script>
+        export default{
+            data(){
+                return {
+                    username: 'admin',
+                    age: 18
+                }
+            },
+            methods: {
+                cn(){
+                    console.log('cn');
+                    console.log(this);
+                    this.username = '改变的名字'
+
+                }
+            }
+        }
+    </script>
+<style lang='less'>
+    div {
+        background-color: pink;
+        text-align: center;
+        button {
+            background-color: skyblue;
+            border: none;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+// 导入 vue 这个包 ，得到 vue 构造函数
 import Vue from 'vue'
-import App from './App.vue'
+// 导入 app.vue 根组件， 将来要把 App.vue 中的模板结构渲染到 HTML 页面中
+// import App from './App.vue'
+import App from './Test.vue'
 
 Vue.config.productionTip = false
 
+// 创建 Vue 实例对象
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+  render: h => h(App),// 把 render 函数指定的组件渲染到 HTML 页面中
+
+}).$mount('#app') // 等价于 el: '#app'
